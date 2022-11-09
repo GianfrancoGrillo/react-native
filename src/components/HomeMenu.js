a42820a964312c7cdfc27e6523e5cad2ac60cb49
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-11-09 16:49:28 -0300

    empezando camara

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
+//import { FontAwesome } from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
+//import NewPost from '../screens/NewPost';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -10,8 +12,9 @@ const Tab = createBottomTabNavigator();
 function HomeMenu() {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name="Home" component={Home} />
-			<Tab.Screen name="Profile" component={Profile} />
+			<Tab.Screen name="Home" component={Home}/> {/*  options={{ tabBarIcon: () => <FontAwesome name="home" size={24} color="black" /> }} */}
+			<Tab.Screen name="Profile" component={Profile} /> {/* options={{ tabBarIcon: () => <FontAwesome name="photo" size={24} color="black" /> }}   */}
+           {/*  <Tab.Screen name="NewPost" component={NewPost} /> */}{/* options={{ tabBarIcon: () => <FontAwesome name="user" size={24} color="black" /> }}  */}
 		</Tab.Navigator>
 	);
 }
